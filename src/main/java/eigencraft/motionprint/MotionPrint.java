e06b409015848b6035fcc12a32fb17b7dfdd6ea0
AMereBagatelle <themerebagatelle@gmail.com>
2020-06-16 16:31:21 -0400

    Initial Commit

---
+++ src/main/java/eigencraft/motionprint/MotionPrint.java
@@ -0,0 +1,12 @@
+package eigencraft.motionprint;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class MotionPrint implements DedicatedServerModInitializer {
+
+    @Override
+    public void onInitializeServer() {
+
+    }
+}
