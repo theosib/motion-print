ed0bc16a0186df9332d00a0359dc2523d9128240
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-06-18 00:17:14 +0300

    Add the sneaking status to the logged data

--- src/main/java/eigencraft/motionprint/data/PlayerStatusData.java
+++ src/main/java/eigencraft/motionprint/data/PlayerStatusData.java
@@ -12,25 +12,28 @@ public class PlayerStatusData {
     protected final float yaw;
     protected final float pitch;
     protected final long worldTick;
+    protected final boolean sneaking;
     protected final boolean onGround;
 
-    public PlayerStatusData(DimensionType dim, Vec3d velocity, Vec3d pos, float yaw, float pitch, long worldTick, boolean onGround) {
+    public PlayerStatusData(DimensionType dim, Vec3d velocity, Vec3d pos, float yaw, float pitch, long worldTick, boolean sneaking, boolean onGround) {
         this.dim = dim;
         this.velocity = velocity;
         this.pos = pos;
         this.yaw = yaw;
         this.pitch = pitch;
         this.worldTick = worldTick;
+        this.sneaking = sneaking;
         this.onGround = onGround;
     }
 
     public String formatData() {
         Vec3d pos = this.pos;
         Vec3d vel = this.velocity;
-        String onGround = this.onGround ? "G" : "A"; // ground vs. air
+        String onGroundStr = this.onGround ? "G" : "A"; // ground vs. air
+        String sneakingStr = this.sneaking ? "S" : "-"; // sneaking vs. not sneaking
 
-        return String.format("%d p[%.4f %.4f %.4f] v[%.3f %.3f %.3f] %.4f %.4f %s\n",
-                             this.worldTick, pos.x, pos.y, pos.z, vel.x, vel.y, vel.z, this.yaw, this.pitch, onGround);
+        return String.format("%d p[%.4f %.4f %.4f] v[%.3f %.3f %.3f] %.4f %.4f %s %s\n",
+                             this.worldTick, pos.x, pos.y, pos.z, vel.x, vel.y, vel.z, this.yaw, this.pitch, onGroundStr, sneakingStr);
     }
 
     public static PlayerStatusData of(PlayerEntity player) {
@@ -38,6 +41,6 @@ public class PlayerStatusData {
 
         return new PlayerStatusData(world.getDimension().getType(),
                                     player.getVelocity(), player.getPos(), player.getYaw(0f), player.getPitch(0f),
-                                    world.getTime(), player.onGround);
+                                    world.getTime(), player.isSneaking(), player.onGround);
     }
 }
