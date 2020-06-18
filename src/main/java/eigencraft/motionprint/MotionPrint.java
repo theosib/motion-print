2b66ead67d8c760fbbb6981d048c698fde7935c2
AMereBagatelle <themerebagatelle@gmail.com>
2020-06-18 06:58:28 -0400

    Basic consent system done, needs modularity

--- src/main/java/eigencraft/motionprint/MotionPrint.java
+++ src/main/java/eigencraft/motionprint/MotionPrint.java
@@ -2,9 +2,11 @@ package eigencraft.motionprint;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.network.ServerPlayerEntity;
 
 public class MotionPrint implements DedicatedServerModInitializer {
     public static boolean hasConsented = true;
+    public static ServerPlayerEntity currentPlayer = null;
 
     @Override
     public void onInitializeServer() {
--- src/main/java/eigencraft/motionprint/mixin/PlayerManagerMixin.java
+++ src/main/java/eigencraft/motionprint/mixin/PlayerManagerMixin.java
@@ -1,11 +1,13 @@
 package eigencraft.motionprint.mixin;
 
+import eigencraft.motionprint.MotionPrint;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.MessageType;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,6 +17,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class PlayerManagerMixin {
     @Inject(method = "onPlayerConnect", at = @At("TAIL"))
     public void onPlayerConnect(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
+        MotionPrint.currentPlayer = player;
         player.sendChatMessage(new LiteralText("Do you consent? Respond with \"yes\" or \"no\"."), MessageType.SYSTEM);
+    }
+
+    @Inject(method = "broadcastChatMessage", at = @At("TAIL"))
+    public void onChatMessage(Text text, boolean system, CallbackInfo ci) {
+        String message = text.asFormattedString().substring(text.asFormattedString().indexOf(">")+2);
+        if(!MotionPrint.hasConsented) {
+            if (message.equalsIgnoreCase("yes")) {
+                MotionPrint.currentPlayer.sendChatMessage(new LiteralText("Thanks for your consent!"), MessageType.SYSTEM);
+                MotionPrint.hasConsented = true;
+            } else if (message.equalsIgnoreCase("no")) {
+                MotionPrint.currentPlayer.sendChatMessage(new LiteralText("No?"), MessageType.SYSTEM);
+            } else {
+                MotionPrint.currentPlayer.sendChatMessage(new LiteralText("Please answer \"yes\" or \"no\"."), MessageType.SYSTEM);
+            }
+        }
     }
 }
