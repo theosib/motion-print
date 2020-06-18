ca457852feb5d3633e18d1203d85692ed5064be6
AMereBagatelle <themerebagatelle@gmail.com>
2020-06-18 07:41:20 -0400

    Making the system work with multiple players

--- src/main/java/eigencraft/motionprint/MotionPrint.java
+++ src/main/java/eigencraft/motionprint/MotionPrint.java
@@ -4,9 +4,11 @@ import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.server.network.ServerPlayerEntity;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 public class MotionPrint implements DedicatedServerModInitializer {
-    public static boolean hasConsented = true;
-    public static ServerPlayerEntity currentPlayer = null;
+    public static LinkedHashMap<String, Boolean> playersNameHasConsented = new LinkedHashMap<>();
 
     @Override
     public void onInitializeServer() {
--- src/main/java/eigencraft/motionprint/mixin/PlayerManagerMixin.java
+++ src/main/java/eigencraft/motionprint/mixin/PlayerManagerMixin.java
@@ -3,37 +3,50 @@ package eigencraft.motionprint.mixin;
 import eigencraft.motionprint.MotionPrint;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.MessageType;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
+    @Shadow @Final private MinecraftServer server;
+
     @Inject(method = "onPlayerConnect", at = @At("TAIL"))
     public void onPlayerConnect(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
-        MotionPrint.currentPlayer = player;
+        MotionPrint.playersNameHasConsented.put(player.getName().asFormattedString(), false);
         player.sendChatMessage(new TranslatableText("consent.askMessage"), MessageType.SYSTEM);
     }
 
     @Inject(method = "broadcastChatMessage", at = @At("TAIL"))
     public void onChatMessage(Text text, boolean system, CallbackInfo ci) {
-        String message = text.asFormattedString().substring(text.asFormattedString().indexOf(">")+2);
-        if(!MotionPrint.hasConsented) {
-            if (message.equalsIgnoreCase("yes")) {
-                MotionPrint.currentPlayer.sendChatMessage(new LiteralText("consent.replyToYes"), MessageType.SYSTEM);
-                MotionPrint.hasConsented = true;
-            } else if (message.equalsIgnoreCase("no")) {
-                MotionPrint.currentPlayer.sendChatMessage(new TranslatableText("consent.replyToNo"), MessageType.SYSTEM);
-            } else {
-                MotionPrint.currentPlayer.sendChatMessage(new TranslatableText("consent.replyToOther"), MessageType.SYSTEM);
-            }
+        if(!system) {
+            String playerName = text.asFormattedString().substring(text.asFormattedString().indexOf("<")+1, text.asFormattedString().indexOf(">"));
+            String message = text.asFormattedString().substring(text.asFormattedString().indexOf(">") + 2);
+            MotionPrint.playersNameHasConsented.forEach((name, consentStatus) -> {
+                if (playerName.equals(name) && !consentStatus) {
+                    ServerPlayerEntity targetPlayer = server.getPlayerManager().getPlayer(playerName);
+                    if (targetPlayer != null) { // Sanity check
+                        if (message.equalsIgnoreCase("yes")) {
+                            targetPlayer.sendChatMessage(new LiteralText("consent.replyToYes"), MessageType.SYSTEM);
+                            MotionPrint.playersNameHasConsented.replace(name, true);
+                        } else if (message.equalsIgnoreCase("no")) {
+                            targetPlayer.sendChatMessage(new TranslatableText("consent.replyToNo"), MessageType.SYSTEM);
+                        } else {
+                            targetPlayer.sendChatMessage(new TranslatableText("consent.replyToOther"), MessageType.SYSTEM);
+                        }
+                    }
+                }
+            });
         }
     }
 }
